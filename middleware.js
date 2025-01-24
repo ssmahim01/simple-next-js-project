acd5b45519f43b12e59b44625543f233c01239cb
ssmahim01 <mahimzayn666@gmail.com>
2025-01-24 14:14:42 +0600

    Added AuthProvider and middleware file for check authentication globally also wrap app in the AuthProvider component

---
+++ middleware.js
@@ -0,0 +1,19 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function middleware(req) {
+  const session = await getKindeServerSession(req);
+
+  if (!session.isAuthenticated()) {
+    const loginUrl = new URL("/api/auth/login", req.url);
+    loginUrl.searchParams.set("post_login_redirect_url", req.nextUrl.pathname);
+    return NextResponse.redirect(loginUrl);
+  }
+
+  return NextResponse.next();
+}
+
+// Protect Profile route
+export const config = {
+  matcher: ["/profile"],
+};
--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { AuthProvider } from "./AuthProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,6 +21,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <AuthProvider>
       <html lang="en">
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
@@ -29,5 +31,6 @@ export default function RootLayout({ children }) {
           <Footer />
         </body>
       </html>
+    </AuthProvider>
   );
 }
